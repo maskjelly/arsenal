5ce448d21e8bacbf41a8aeb079bda5d1bb588ede
asdfghjA1 <aaryan14decembersingh@gmail.com>
2024-10-27 05:07:30 +0530

    initial bad launch updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+    <meta charSet="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="twitter:card" content="rr.svg"/>
+    <meta name="twitter:site" content="@liquidzooo"/>
+    <meta name="twitter:title" content="BEANLABS"/>
+    <meta name="twitter:description" content="AI CORE answer enginer for RESEARCHERS"/>
+    <meta name="twitter:image" content="https://beanlabs.vercel.app/rr.svg"/>
+    <meta name="twitter:image:alt" content="AI answer engine for research"/>
+    <title>Your Page Title</title>
+</head>
       <body className="font-mono">
         <Banner/>
         <ThemeProvider
