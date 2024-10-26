093ee7bc7c05fbab5fa19b8f4fb7a89de6913e15
asdfghjA1 <aaryan14decembersingh@gmail.com>
2024-10-27 05:23:25 +0530

    summary_large_image for twitter::card

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <head>
     <meta charSet="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta name="twitter:card" content="rr.svg"/>
+    <meta name="twitter:card" content="summary_large_image"/>
     <meta name="twitter:site" content="@liquidzooo"/>
     <meta name="twitter:title" content="ResearchRelay"/>
     <meta name="twitter:description" content="AI CORE answer enginer for RESEARCHERS"/>
