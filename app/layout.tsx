f0d0c3532c53ce6ab046ea2617b55c107e5ce451
asdfghjA1 <aaryan14decembersingh@gmail.com>
2024-10-27 23:17:53 +0530

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/app/theme-provider";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Banner from "@/components/banner";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,24 +19,30 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-    <meta charSet="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta name="twitter:card" content="summary_large_image"/>
-    <meta name="twitter:site" content="@liquidzooo"/>
-    <meta name="twitter:title" content="ResearchRelay"/>
-    <meta name="twitter:description" content="AI CORE answer enginer for RESEARCHERS"/>
-    <meta name="twitter:image" content="https://beanlabs.vercel.app/rr.svg"/>
-    <meta name="twitter:image:alt" content="AI answer engine for research"/>
-    <meta name="twitter:url" content="https://beanlabs.vercel.app"/>
-    <title>ResearchRelay</title>
-</head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@liquidzooo" />
+        <meta name="twitter:title" content="ResearchRelay" />
+        <meta
+          name="twitter:description"
+          content="AI CORE answer enginer for RESEARCHERS"
+        />
+        <meta
+          name="twitter:image"
+          content="https://beanlabs.vercel.app/rr.svg"
+        />
+        <meta
+          name="twitter:image:alt"
+          content="AI answer engine for research"
+        />
+        <meta name="twitter:url" content="https://beanlabs.vercel.app" />
+        <title>ResearchRelay</title>
+        <Analytics />
+      </head>
       <body className="font-mono">
-        <Banner/>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-        >
+        <Banner />
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
         </ThemeProvider>
       </body>
