668d6948e26042f97a3e34feb1b5ced3b2136a7e
asdfghjA1 <aaryan14decembersingh@gmail.com>
2024-10-30 07:42:41 +0530

    Fullported - fixed broken linting errors

--- app/actions/action.ts
+++ app/actions/action.ts
@@ -43,3 +43,45 @@ export async function searchTavily(query: string): Promise<SearchResult> {
     }
   }
 }
+/**
+ * 
+ * // import { openai } from '@ai-sdk/openai';
+// import { convertToCoreMessages, streamText } from 'ai';
+// import { tavily } from '@tavily/core';
+// const APIKEY = process.env.TAVAPI;
+// const tvly = tavily({ apiKey: APIKEY });
+
+// // Allow streaming responses up to 30 seconds
+// export const maxDuration = 60;
+
+// export async function POST(req: Request) {
+//   // Extract user messages from the request payload
+//   const { messages } = await req.json();
+
+//   // Extract the user's last message (the latest question or input)
+//   const lastMessage = messages[messages.length - 1]?.content;
+
+//   // Fetch relevant context using Tavily based on the user's last message
+//   const context = await tvly.searchContext(lastMessage , {includeImages : true})
+//   // Now i have to change the context to the new REST API for the context and also for the image 
+  
+  
+//   // Prepare combined input with the user's message and retrieved context
+//   const combinedMessages = [
+//     ...messages,
+//     context
+//   ];
+
+//   // Stream the result using OpenAI's GPT model
+//   const result = await streamText({
+//     model: openai('gpt-4-turbo'),
+//     system: `As a professional search master , you can output in BEAUTIFUL STYLISH MARKDOWN OUTPUT STRICTLY and with ANY LINKS and their PREVIEW POSSIBLE , you possess the ability to search for any information on the web. For each user query, utilize the search results to their fullest potential to provide additional information and assistance in your response. Summarize and answer based on what you have been asked.`,
+//     messages: convertToCoreMessages(combinedMessages),
+//   });
+
+//   // Return the result as a streaming response
+//   return result.toDataStreamResponse();
+// }
+
+
+ */
